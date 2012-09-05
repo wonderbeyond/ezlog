67857b92b127eb69bdc9a3dee477d4d76ecf7440
Wonder <wonderbeyond@gmai.com>
2012-09-05 23:15:51 +0800

    删除多余文件

--- blog/_views.py
+++
@@ -1,8 +0,0 @@
-# coding=utf-8
-from django.conf import settings
-from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseRedirect, HttpResponseForbidden
-from django.shortcuts import render_to_response, get_object_or_404
-from django.template import RequestContext, Context, Template
-from django.http import QueryDict
-
-from blog.models import *
